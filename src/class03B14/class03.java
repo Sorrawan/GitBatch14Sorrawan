5d8d17b746a2939b00a7f4ecf9cc6505988fe4c4
Sorrawan <sorrawan12@gmail.com>
2022-09-27 20:07:42 -0400

    third commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -7,6 +7,7 @@ public class class03 {
 		
 		System.out.println("hey this is a git class");
 		System.out.println("hey this is a change in my projecct");
+		System.out.println("how are you this a new version");
 		
 	}
 
